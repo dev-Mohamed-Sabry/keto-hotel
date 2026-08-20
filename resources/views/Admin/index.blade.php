1b3763dc7f8107823a1ce02cb9c4a1c225f8ceca
MS <mo7ammed.sabre@gmail.com>
2026-08-20 16:14:57 +0300

    Admin Dashboard View Code Refactoring

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -455,9 +455,14 @@
         <footer class="footer">
             <div class="footer__block block no-margin-bottom">
                 <div class="container-fluid text-center">
-                    <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
-                    <p class="no-margin-bottom">2018 &copy; Your company. Download From <a target="_blank"
-                            href="https://templateshub.net">Templates Hub</a>.</p>
+                    <p class="no-margin-bottom">
+                        &copy; {{ date('Y') }} Keto Hotel. All Rights Reserved.
+                        <br>
+                        Developed by
+                        <a href="https://www.linkedin.com/in/mo-sabre" target="_blank">
+                            Mohammed Sabry
+                        </a>
+                    </p>
                 </div>
             </div>
         </footer>
--- resources/views/Dashboard/Layouts/admin_dashboard.blade.php
+++ resources/views/Dashboard/Layouts/admin_dashboard.blade.php
@@ -21,7 +21,7 @@
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="{{ asset('assets/Admin_Dashboard/css/custom.css') }}">
     <!-- Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('assets/Website/images/logo.png') }}">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
